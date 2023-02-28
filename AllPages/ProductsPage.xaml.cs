e8d6b9408490237c0d0075328a2ca31ab3a3ab7b
ArchSeib <ruslan.bakybaev@mail.ru>
2023-03-01 00:58:56 +0700

    редактирование 01.05.23 0:58

--- AllPages/ProductsPage.xaml.cs
+++ AllPages/ProductsPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Trade.AllClass;
 
 namespace Trade.AllPages
 {
@@ -20,9 +21,80 @@ namespace Trade.AllPages
     /// </summary>
     public partial class ProductsPage : Page
     {
+        private static List<Product> productItems = new List<Product>(); 
         public ProductsPage()
         {
             InitializeComponent();
+            if (Helper.Role == "Клиент")
+            {
+                BtnNewOrder.Visibility = Visibility.Visible;
+            }
+            if (Helper.Role == "Админ")
+            {
+                BtnDeleteProduct.Visibility = Visibility.Visible;
+                BtnEditProduct.Visibility = Visibility.Visible;
+                BtnNewOrder.Visibility = Visibility.Visible;
+                BtnNewProduct.Visibility = Visibility.Visible;
+            }
+            Load();
+        }
+        private static void CreateProductList()
+        {
+            foreach(Product item in Helper.GetData().Product.ToList())
+            {
+                if (item.ProductDiscountAmount >= 15)
+                {
+                    item.ColorCodeDiscount = 1;
+                }
+                else
+                {
+                    item.ColorCodeDiscount = 0;
+                }
+                item.ProductRealCost = item.ProductCost- item.ProductCost*Convert.ToDecimal(item.ProductDiscountAmount);
+                if (item.ProductPhoto == null)
+                {
+                    item.PathPhoto = new BitmapImage(new Uri("/Resources/picture.png", UriKind.Relative));
+                }
+                else
+                {
+                    item.PathPhoto = new BitmapImage(new Uri(item.ProductPhoto, UriKind.Relative));
+                }
+                productItems.Add(item);
+            }
+        }
+        private void Load()
+        {
+            CreateProductList();
+            LVProducts.ItemsSource = productItems;
+        }
+        private void CbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TbSeacrh_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnNewOrder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- Product.cs
+++ Product.cs
@@ -26,46 +26,12 @@ namespace Trade
         public string ProductDescription { get; set; }
         public string ProductCategory { get; set; }
         public string ProductPhoto { get; set; }
-        public BitmapImage PathPhoto
-        {
-            set
-            {
-                if (ProductPhoto == null)
-                {
-                    this.PathPhoto = new BitmapImage(new Uri("/Resources/picture.png", UriKind.RelativeOrAbsolute));
-                }
-                else
-                {
-                    this.PathPhoto = new BitmapImage(new Uri(ProductPhoto, UriKind.Relative));
-                }
-            }
-            get { return this.PathPhoto; }
-        }
+        public BitmapImage PathPhoto { get; set; }
         public string ProductManufacturer { get; set; }
         public decimal ProductCost { get; set; }
-        public decimal CostNow {
-            get { return this.CostNow; }
-            set
-            {
-                CostNow = ProductCost - ProductCost*Convert.ToDecimal(ProductDiscountAmount);
-            }
-        }
+        public decimal ProductRealCost { get; set; }
         public Nullable<byte> ProductDiscountAmount { get; set; }
-        public Int32 ColorCodeDiscount
-        {
-            set
-            {
-                if (ProductDiscountAmount >= 15)
-                {
-                    ColorCodeDiscount = 1;
-                }
-                else
-                {
-                    ColorCodeDiscount = 0;
-                }
-            }
-            get { return this.ColorCodeDiscount; }
-        }
+        public Int32 ColorCodeDiscount { get; set; }
         public int ProductQuantityInStock { get; set; }
         public string ProductStatus { get; set; }
         public string Unit { get; set; }
